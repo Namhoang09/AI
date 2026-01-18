6e3a574591975e8d8491c3eca8e490e740b826e4
Đặng Hoàng Nam <138839936+Namhoang09@users.noreply.github.com>
2026-01-18 22:05:44 +0700

    Update check_module.py

--- check_module.py
+++ check_module.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-GOOGLE_API_KEY = "AIzaSyBm4ijudeXHI3_K6MEzshWklKRdCTt1E3k"
+GOOGLE_API_KEY = "..."
 genai.configure(api_key=GOOGLE_API_KEY)
 
 print("Đang hỏi Google danh sách model...")
@@ -9,4 +9,5 @@ try:
         if 'generateContent' in m.supported_generation_methods:
             print(f"- {m.name}")
 except Exception as e:
+
     print(f"Lỗi: {e}")
